d7de3ab95331fe9b7e6c38260f7ee239452f8376
vako32124 <vakochologauri@gamil.com>
2025-05-04 12:39:38 +0400

    added booking

--- src/app/booked/booked.component.ts
+++ src/app/booked/booked.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ApiservicesService } from '../services/apiservices.service';
 
 @Component({
   selector: 'app-booked',
@@ -11,13 +12,24 @@ import { FormsModule } from '@angular/forms';
 export class BookedComponent {
 
   bookings: any[] = [];
+ constructor(private api : ApiservicesService){}
+
 
   ngOnInit(): void {
     let storedBookings = localStorage.getItem('bookings');
     this.bookings = storedBookings ? JSON.parse(storedBookings) : [];
   }
-  deleteBooking(){
-    
+  deleteBooking(booking: any): void {
+    let stored = localStorage.getItem('bookings');
+    let bookings = stored ? JSON.parse(stored) : [];
+
+    let bookingId = booking.customerId || booking.id;
+    let index = bookings.findIndex((b: { customerId: any; id: any; }) => (b.customerId || b.id) === bookingId);
+    if (index !== -1) {
+      bookings.splice(index, 1);
+      localStorage.setItem('bookings', JSON.stringify(bookings));
+      this.bookings = bookings;
+    }
   }
   
 }
