37973d91148d96bf5e3f62e80ce826d798cf278e
vako32124 <vakochologauri@gamil.com>
2025-05-11 13:15:53 +0400

    .

--- src/app/booked/booked.component.ts
+++ src/app/booked/booked.component.ts
@@ -17,39 +17,39 @@ export class BookedComponent {
   constructor(private api : ApiservicesService){}
 
 
-  //ngOnInit(): void {
-   // let storedBookings = localStorage.getItem('bookings');
-   // this.bookings = storedBookings ? JSON.parse(storedBookings) : [];
- // }
-//  deleteBooking(booking: any): void {
- //   Swal.fire({
- //     title: 'Are you sure?',
- //     text: "You won't be able to revert this!",
-  //    icon: 'warning',
-  //    showCancelButton: true,
- //     confirmButtonColor: '#3085d6',
- //     cancelButtonColor: '#d33',
- //     confirmButtonText: 'Yes, delete it!'
- //   }).then((result: { isConfirmed: any; }) => {
-//      if (result.isConfirmed) {
-  //      let stored = localStorage.getItem('bookings');
-  //      let bookings = stored ? JSON.parse(stored) : [];
+  ngOnInit(): void {
+    let storedBookings = localStorage.getItem('bookings');
+   this.bookings = storedBookings ? JSON.parse(storedBookings) : [];
+  }
+  deleteBooking(booking: any): void {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+     cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result: { isConfirmed: any; }) => {
+       if (result.isConfirmed) {
+        let stored = localStorage.getItem('bookings');
+        let bookings = stored ? JSON.parse(stored) : [];
 
-   //     let bookingId = booking.customerId || booking.id;
- //       let index = bookings.findIndex((b: { customerId: any; id: any; }) => (b.customerId || b.id) === bookingId);
- //       if (index !== -1) {
-   //       bookings.splice(index, 1);
-  //        localStorage.setItem('bookings', JSON.stringify(bookings));
-   //       this.bookings = bookings;
-   //       Swal.fire(
-   //         'Deleted!',
-   //         'Your booking has been deleted.',
-    //        'success'
-   //       );
-    //    }
-    //  }
-  //  });
-  //}
+        let bookingId = booking.customerId || booking.id;
+       let index = bookings.findIndex((b: { customerId: any; id: any; }) => (b.customerId || b.id) === bookingId);
+        if (index !== -1) {
+         bookings.splice(index, 1);
+          localStorage.setItem('bookings', JSON.stringify(bookings));
+          this.bookings = bookings;
+          Swal.fire(
+            'Deleted!',
+            'Your booking has been deleted.',
+            'success'
+          );
+        }
+      }
+    });
+  }
 
   deleteBookingId: string = '';
 
